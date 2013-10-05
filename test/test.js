2edaa4516cb43c4e336d48cc14f741ba101bc754
Mikola Lysenko <mikolalysenko@gmail.com>
2013-10-05 18:45:51 -0500

    added more test cases

--- test/test.js
+++ test/test.js
@@ -13,5 +13,13 @@ require("tape")(function(t) {
   
   t.equals(lr([100, 100], [1, 1], [30, 0]), 1)
   
+  for(var i=-100; i<=100; i+=5) {
+    t.equals(lr([0,0], [1, Math.pow(2,i)], [Math.pow(2,-i), 1]), 0)
+    for(var j=-100; j<=100; j+=5) {
+      t.equals(lr([0,0], [1, Math.pow(2,i)], [Math.pow(2,j), 0]), -1)
+      t.equals(lr([0,0], [1, Math.pow(2,i)], [-Math.pow(2,j), 0]), 1)
+    }
+  }
+  
   t.end()
 })
